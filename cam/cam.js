55a88a5e13ed6bb6fb4f07452b43834832be9093
IshowSpeed614 <119762108+IshowSpeed614@users.noreply.github.com>
2022-12-04 11:52:49 +0000

    Add files via upload

--- cam/cam.js
+++ cam/cam.js
@@ -1,2 +1,12 @@
-var video = document.querySelector("#videoElement"); if (navigator.mediaDevices.getUserMedia) { navigator.mediaDevices.getUserMedia({ video: true }) .then(function (stream) { video.srcObject = stream; }) .catch(function (err0r) { console.log("Something went wrong!"); }); }
-
+function stream (video){ 
+ alert("hello started streaming!"); 
+ if (navigator.mediaDevices.getUserMedia) { 
+     navigator.mediaDevices.getUserMedia({ video: true }) 
+       .then(function (stream) { 
+         video.srcObject = stream; 
+       }) 
+       .catch(function (err0r) { 
+         console.log("Something went wrong!"); 
+       }); 
+   } 
+ } 
